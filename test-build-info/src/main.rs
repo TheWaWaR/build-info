5efebb81350860962033cda383b259e0ce0e86da
Qian Linfeng <thewawar@gmail.com>
2018-04-03 00:09:12 +0800

    Simplify API

--- test-build-info/src/main.rs
+++ test-build-info/src/main.rs
@@ -1,14 +1,17 @@
-
 extern crate clap;
 
 include!(concat!(env!("OUT_DIR"), "/build_info.rs"));
 
 fn main() {
-
     let _ = clap::App::new("Test build.rs program")
         .version(get_build_info_str(true))
         .long_version(get_build_info_str(false))
         .get_matches();
 
-    println!("build_info: {:?}", get_build_info());
+    println!("get_build_info_str(true): {}", get_build_info_str(true));
+    println!("get_build_info_str(false): {}", get_build_info_str(false));
+
+    let info = get_build_info();
+    println!("build_info: {:?}", info);
+    println!("logo:\n{}", info.0)
 }
