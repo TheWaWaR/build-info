53df95fe353a6b6a639049cf41295735eaa5e4bd
Qian Linfeng <thewawar@gmail.com>
2018-04-02 16:29:41 +0800

    Add `git describe --dirty=-dev` support

--- test-build-info/src/main.rs
+++ test-build-info/src/main.rs
@@ -6,7 +6,8 @@ include!(concat!(env!("OUT_DIR"), "/build_info.rs"));
 fn main() {
 
     let _ = clap::App::new("Test build.rs program")
-        .version(get_build_info_str())
+        .version(get_build_info_str(true))
+        .long_version(get_build_info_str(false))
         .get_matches();
 
     println!("build_info: {:?}", get_build_info());
